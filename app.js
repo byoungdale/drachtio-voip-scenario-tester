cb16d1c86afb6049aa458d4160fd8032b1c3f2f8
byoungdale <brandonyoungdale@gmail.com>
2017-04-17 12:42:59 -0700

    fixed mediaserver connection issue

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var debug = require('debug');
 const registrationHandler = require('./lib/registrationHandler');
 const callHandler = require('./lib/callHandler');
 
+
+const opts = {
+  domain: 'sip.phone.com',
+  user: 	120347,
+  password: 'Ty5rq4vP3@m7c2',
+  hostport: 5060,
+  callednumber: 17606590215,
+}
+
+/*
 const opts = {
   domain: 'sip.qa.phone.com',
   user: 	124331,
@@ -18,7 +28,7 @@ const opts = {
   hostport: 5060,
   callednumber: 17606590215,
 }
-
+*/
 const drachtioConnectOpts = {
   host: 'localhost',
   port: 8022,
@@ -30,28 +40,30 @@ const mediaserverConnectOpts = {
   port: 8021,
   secret: 'ClueCon',
   listenAddress: '127.0.0.1',
-  listenPort: 8085
+  listenPort: 8081
 } ;
 
 srf.connect(drachtioConnectOpts) ;
 
-mrf.connect(mediaserverConnectOpts, (err, mediaServer) => {
-  if (err) { debug(err) ; }
-  mediaServer.on('connect', (err, ms) => {
-    if (err) { debug(err); }
-    console.log('connected to media server listening on %s:%s', ms.sipAddress, ms.sipPort) ;
-    MediaServices.addMediaServer( ms ) ;
-    });
-  }) ;
+mrf.connect(mediaserverConnectOpts, (ms) => {
+  console.log(`finally connected to mediaserver ${JSON.stringify(ms)}`);
+  MediaServices.MediaResources.addMediaServer( ms ) ;
+});
 
 srf.on('connect', (err, hostport) => {
   console.log('connected to drachtio listening for SIP on %s', hostport) ;
 }) ;
 
+mrf.on('connect', (ms) => {
+  if (err) { debug(err); }
+  console.log('connected to media server listening on %s:%s', ms.sipAddress, ms.sipPort) ;
+  MediaServices.addMediaServer( ms ) ;
+});
+
 
 // registerUser function's callback receives expires variable
 // from the 200 OK back from the registrar
-/*
+
 registrationHandler.register(opts, srf, (opts, srf, expires) => {
   console.log(`re-registering`);
   srf.request(`sip:${opts.user}@${opts.domain}`, {
@@ -69,4 +81,3 @@ registrationHandler.register(opts, srf, (opts, srf, expires) => {
 });
 
 callHandler.receiveCall(srf);
-*/
