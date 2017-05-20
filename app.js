bb4796dc4cbd59c0d879238a01e0e8ebc38382b1
byoungdale <brandonyoungdale@gmail.com>
2017-05-19 19:22:41 -0700

    re-factored to use Promises instead of callbacks

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const MediaServices = require('./lib/media-services') ;
 var debug = require('debug');
 const registrationHandler = require('./lib/registrationHandler');
 const async = require('async');
-const Call = require('./lib/call');
-const incall = require('./lib/incall');
+
 const config = require('./config');
+const Call = require('./lib/call');
+const call = new Call(config, srf);
+const incall = require('./lib/incall');
 
 srf.connect(config.drachtioConnectOpts) ;
 
@@ -34,23 +36,10 @@ mrf.on('connect', (ms) => {
 // registerUser function's callback receives expires variable
 // from the 200 OK back from the registrar
 
-registrationHandler.register(config.user, srf, (opts, srf, expires) => {
-  console.log(`re-registering`);
-  srf.request(`sip:${opts.username}@${opts.domain}`, {
-      method: 'REGISTER',
-      headers: {
-          'Expires': expires,
-          'From': `sip:${opts.username}@${opts.domain}`,
-          'Contact': `sip:${opts.username}@${opts.domain}:${opts.hostport}`
-      },
-      auth: {
-        username: opts.username,
-        password: opts.password
-      }
+registrationHandler.register(config.user, srf, '3600')
+  .then((result) => {
+    registrationHandler.register(result.opts, result.srf, result.expires);
   });
-});
-
-const call = new Call(config, srf);
 
 // ideal final setup
 // scenario.run(scenarioOpts)
@@ -62,43 +51,24 @@ scenarioOpts.map((test) => {
 });
 */
 
-// An example of creating scenarios out of the call and incall functions
-// call functions would be within a async.series because they don't need
-// to pass their results to the net function.
-// But, the incall functions would need to pass the results to the next
-// function, so we can use a async.waterfall to pass dialog and endpoint
-// parameters along the call.
-/*
-async.series({
-  call.getEndpoint(config.user, (ep) => {
-    call.send(srf, ep, '1000', config.user, (err, ep, dialog) => {
-      if (err) { throw err; }
-      async.waterfall([
-        incall.playRecording(ep, dialog, ['ivr/8000/ivr-oh_whatever.wav']);
-        setTimeout(() => { incall.sendDTMF(ep, dialog, '*2'); }, 5000);
-        setTimeout(() => { incall.sendDTMF(ep, dialog, '7609946034'); }, 6000);
-      ]); // async.waterfall
-    }); // call.send
-  }); // call.getEndpoint
-}); // async.series
-*/
-
-
-setTimeout(() => {
-  call.getEndpoint(config.user, (ep) => {
-    call.send(srf, ep, '1000', config.user, (err, ep, dialog) => {
-      if (err) { throw err; }
-      incall.playRecording(ep, dialog, ['ivr/8000/ivr-oh_whatever.wav']);
-    }); // call.send
-  }); // call.getEndpoint
-}, 2000);
-
-/*
-call.receive(config.user, srf, (req, res, config.user) => {
-  call.connect(req, res, config.user, (ep, dialog) => {
-    incall.playRecording(ep, dialog, ['ivr/8000/ivr-oh_whatever.wav']);
-    setTimeout(() => { incall.sendDTMF(ep, dialog, '*2'); }, 5000);
-    setTimeout(() => { incall.sendDTMF(ep, dialog, '7609946034'); }, 6000);
+// call.receive with promises
+call.receive(config.user, srf)
+  .then((result) => {
+    call.connect(result.req, result.res, result.opts)
+    .then((result) => {
+      incall.playRecording(result.ep, result.dialog, ['ivr/8000/ivr-oh_whatever.wav']);
+    });
   });
-});
+
+/*
+// call.send with promises
+setTimeout(() => {
+  call.getEndpoint(config.user)
+    .then((result) => {
+      call.send(srf, result.ep, '1000', config.user)
+        .then((result) => {
+          incall.playRecording(result.ep, result.dialog, ['ivr/8000/ivr-oh_whatever.wav']);
+        });
+    });
+}, 2000); // setTimeout
 */
